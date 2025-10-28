a279eee7f639a87f7e6ddadf442cd7b24b5ee2f2
DavyLeFish <126605125+DavyLeFish@users.noreply.github.com>
2025-10-28 16:44:20 +0000

    Update Main Updated.py

--- Main Updated.py
+++ Main Updated.py
@@ -8,43 +8,83 @@ class Main():
         self.n = 0
         self.min = 0
         self.max = 0
+        self.rng = 0
         self.runs = 0
 
     def Start(self):
+        choice = int(input("Enter 1 for specific matrix, 2 for random matrix:\n")
+        if choice == 1:
+            self.SpcfcMtrx()
+        elif choice == 2:
+            self.RndMtrx()
+        else:
+            self.Start()
+
+#Sepcific Matrix options
+    def SpcfcMtrx(self):
+        mtrx = [[int(input("Row entry "+str(i)+" Column entry "+str(j)+"\n")) for i in range(n)] for j in range(n)]
+        choice = int(input("Enter 1"))
+        
+#Random Martrix options
+    def RndMtrx(self):
+        choice = int(input("Enter 0 to return to start, 1 for total invertable matrices, 2 for total eigenvalues, :\n"))
+        if choice == 0:
+            self.Start()
+        elif choice == 1:
+            self.NumOfInvrts()
+        else:
+            Self.RndMtrx()
+
+#Finds how many invertable matrices exist within the givin parameters
+#To make it more optimised, finds symetries that will have determinate of zero
+    def NumOfInvrts(self):
+#Opens MtrxPrt class
+        P=MtrxPrt()
 #User inputs for desired random matrix
         self.n = int(input("What nxn matrix would you like?\n"))
         self.min = int(input("What is the lower bound for the elements value?\n"))
         self.max = int(input("What is the upper bound for the elements value?\n"))
+
 #Sends to internal function to get total of runs
         self.NumOfTests()
-        c=self.runs
-#Opens MtrxPrt class
-        P=MtrxPrt()
+#Finds how many have zero rows and doubles to include zero columns
+        zrwscls=2*((self.rng)**((self.n)**2))-((self.rng)**((self.n-1)**n)))
+#Finds how many have zero diagonal
+        zdgnl=(self.rng)**(((self.n)**2)-self.n)
+#Produces affirmentioned matrices if the total of combinations is small enough where the amount of zero rows and columns would be likely to be tested in the random selection
+#using a boundary of 5% of the total
+        if (zrwscls+zdgnl)/(ttl)<=0.95:
+            for j in range(n):
+                for i in range((self.rng)**(((self.n)**2)-self.n)):
+                    P.RndMtrx()
+                    mtrx=P.GetMtrx()
 #Runs through the set amount of runs
+        c=self.runs
         while c != 0:
-#Calss function to create random matrix
+#Calls function to create random matrix and checks it hasn't already been tested
+            P.RndMtrx(self.n,self.min,self.max)
+        while P.CheckMtrx(True) == False:
             P.RndMtrx(self.n,self.min,self.max)
 #Checks if the matrix has a non-zero determinant and adds 1 to the counter
             if P.CheckDet() == True:
-                P.Tally()
+                P.Tally(2)
             c-=1
 #Final result
         print("Total invertable matrices = ",P.GetTally(),"\nout of a total of ",self.runs,""
         "\n giving a total percentage of invertable ",self.n,"*",self.n," matrices ranging from ",self.min,"-",self.max," ",
         (P.GetTally()/float(self.runs))*100,"%")
 #Loops back to beggning
-        self.Start()
+        self.RndMtrx()        
+
     
     def NumOfTests(self):
 #Gets the range of different available values
-        rng=Abs(self.max-self.min)+1
+        self.rng=Abs(self.max-self.min)+1
 #Gets total of different possible matrices
-        ttl=(rng)**((self.n)**2)
+        ttl=(self.rng)**((self.n)**2)
 #Limits the amount of runs
         if ttl>=10000:
-            self.runs = math.ceil(0.1*ttl)
-        elif ttl>=1000:
-            self.runs = math.ceil(0.5*ttl)
+            self.runs = 10000
         else:
             self.runs = ttl
 
@@ -73,38 +113,48 @@ class MtrxPrt():
     def Eignvls(self):
         return(self.mtrx.eigenvals())
 #Adds one to the tally count    
-    def Tally(self):
-        self.count+=1
+    def Tally(self,n):
+        self.count+=n
 #Returns the tally count
     def GetTally(self):
         return(self.count)
-
-    def RndMtrx(self,n,a,b):
+#Allows creation of personal matrix
+    def CrtMtrx(self,n,mtrx):
+        self.mtrx=Matrix(mtrx)
 #Produces random matrix based on user inputs
-        mtrx=Matrix([[random.randint(a,b) for i in range(n)] for j in range(n)])
+    def RndMtrx(self,n,a,b):
+        self.mtrx=Matrix([[random.randint(a,b) for i in range(n)] for j in range(n)])
 #Checks if the random matrix has already be tested
-        if mtrx in self.lstOFmtrx:
-            self.RndMtrx(n,a,b)
+    def CheckMtrx(self,t):
+        if self.mtrx in self.lstOFmtrx:
+            return False
         else:
 #Adds matrix to list of tested matrices
-            self.lstOFmtrx.append(mtrx)
-            self.mtrx=mtrx
+#if t is true then it also includes the transpose of the matrix
+            if t == False
+                self.lstOFmtrx.append(mtrx)
+                self.mtrx=self.mtrx
+            elif t == True:
+                self.lstOFmtrx.append(self.mtrx.transpose())
+            return True
 
-    def CrtMtrx(self,n):
-#Allows creation of personal matrix
-        mtrx=Matrix([[int(input("Row entry "+str(i)+" Column entry "+str(j)+"\n")) for i in range(n)] for j in range(n)])
-        self.mtrx=mtrx
-        min=None
-        max=None
-#Finds minimum entry in matrix
-        for i in mtrx:
+#Returns the matrix
+    def GetMtrx(self):
+        return(self.mtrx)
+#Returns the inverse matrix
+    def GetInvrtMtrx(self):
+        return(self.invrtmtrx)
+#Returns min value of matrix
+    def MinMtrx(self):
+        for i in self.mtrx:
             if min==None:
                 min=i
             elif i<min:
                 min=i
             else:
                 min=min
-#Finds maximum entry in matrix
+#Finds maximum in matrix
+    def MaxMtrx(self):
         for i in mtrx:
             if max==None:
                 max=i
@@ -113,12 +163,6 @@ class MtrxPrt():
             else:
                 max=max
         return(min,max)
-#Returns the matrix
-    def GetMtrx(self):
-        return(self.mtrx)
-#Returns the inverse matrix
-    def GetInvrtMtrx(self):
-        return(self.invrtmtrx)
 
 class Graph():
     def __init__(self):
@@ -127,5 +171,6 @@ class Graph():
     
 
 Main().Start()
+
 
 
